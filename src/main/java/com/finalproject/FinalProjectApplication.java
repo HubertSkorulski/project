8557db6fb4c12f790bd13585d1d0b62cc4377a15
huubsoon <hubertskorulski@gmail.com>
2022-02-01 21:30:08 +0100

    sd

--- src/main/java/com/finalproject/FinalProjectApplication.java
+++ src/main/java/com/finalproject/FinalProjectApplication.java
@@ -2,9 +2,10 @@ package com.finalproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FinalProjectApplication {
+public class FinalProjectApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinalProjectApplication.class, args);
