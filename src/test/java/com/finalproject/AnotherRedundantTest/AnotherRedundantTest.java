e583b6dd3dabd8ba8b5b9e68e47413617cf0f83a
huubsoon <hubertskorulski@gmail.com>
2022-06-18 18:37:55 +0200

    Another revert test

---
+++ src/test/java/com/finalproject/AnotherRedundantTest/AnotherRedundantTest.java
@@ -0,0 +1,16 @@
+package com.finalproject.AnotherRedundantTest;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AnotherRedundantTest {
+
+    @Test
+    void AnotherRedundantTest() {
+
+        Assertions.assertEquals(1,1);
+    }
+}
