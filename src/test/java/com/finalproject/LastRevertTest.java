6105a3cd66554768667ea5cf0059572101b1cbed
huubsoon <hubertskorulski@gmail.com>
2022-06-18 18:52:48 +0200

    Last try

---
+++ src/test/java/com/finalproject/LastRevertTest.java
@@ -0,0 +1,14 @@
+package com.finalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class LastRevertTest {
+
+    @Test
+    void testTest() {
+        System.out.println("Last testwq");
+    }
+
+}
